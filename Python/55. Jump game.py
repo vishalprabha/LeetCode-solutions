fd3d1bf9ebfede58dac34a50b6305a224a87998c
vishalprabha <vishal.prabhachandar1@gmail.com>
2021-06-18 23:28:48 +0530

    adding question-55

---
+++ Python/55. Jump game.py
@@ -0,0 +1,34 @@
+'''
+Source: https://leetcode.com/problems/jump-game/
+Author: Vishal Prabhachandar
+Date: 2021-June-18
+
+Time complexity: O(n)
+Space complexity: O(1)  
+'''
+
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        '''
+        return is jump is possible 
+
+        :param  nums: List of integers
+        :return boolean
+        '''
+        # forward approach
+        m = 0
+        for index, element in enumerate(nums):
+       
+            if index > element:
+                return(False)
+            m = max(m, index+element)
+        return(True)
+
+        # reverse approach
+        # last_position = len(nums)-1
+        # for i in range(len(nums)-2,-1,-1): 
+        #     if (i + nums[i]) >= last_position: 
+        #         last_position = i 
+        # return(last_position == 0)
+
