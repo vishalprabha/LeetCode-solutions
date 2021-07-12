8210b09fc62a31f5a7f8431dc631e2f5e39a82e2
vishalprabha <vishal.prabhachandar1@gmail.com>
2021-07-12 23:48:41 +0530

    adding additional methods to question-350

--- Python/350. Intersection of two arrays 2.py
+++ Python/350. Intersection of two arrays 2.py
@@ -15,7 +15,6 @@ class Solution:
         :param  nums1: list of integers
         :param  nums2: list of integers
         :return result: list of integers
-        
         '''
         result = []
         for element in nums1:
@@ -24,3 +23,19 @@ class Solution:
                 result.append(element)
                 
         return result
+
+        # inbuilt counter approach
+        # c1, c2 = Counter(nums1), Counter(nums2)
+        # finding intersection and returning
+        # return(c1&c2).elements()
+
+        # dictionary based approach
+        # dic = {}
+        # for element in nums2:
+        #    dic[element] =  dic.get(element,0) + 1
+        # result = []
+        # for element in nums1:
+        #     if element in dic and dic[element] > 0:
+        #         result.append(element)
+        #         dic[element] -= 1
+        # return result
