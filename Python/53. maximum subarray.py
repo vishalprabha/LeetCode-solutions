905067c6b116f5885e972b96a61a78e27351ecec
vishalprabha <vishal.prabhachandar1@gmail.com>
2021-07-17 21:47:28 +0530

    adding kadens approach for question-53

--- Python/53. maximum subarray.py
+++ Python/53. maximum subarray.py
@@ -15,9 +15,15 @@ class Solution:
         :param  nums: list of integers
         :return maximum: integer
         '''
-        current = maximum = nums[0]
+
+        # kadens algorithm approach
         for index in range(1, len(nums)):
-            current = max(nums[index], current + nums[index])
-            maximum = max(current, maximum)
+            if nums[index-1] > 0:
+                nums[index] += nums[index-1]
+        return max(nums)
+        # current = maximum = nums[0]
+        # for index in range(1, len(nums)):
+        #     current = max(nums[index], current + nums[index])
+        #     maximum = max(current, maximum)
             
-        return maximum
+        # return maximum
