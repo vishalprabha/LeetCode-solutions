da3f3afb1cb1607ddd3648aa0aae68c7f8b83a42
vishalprabha <vishal.prabhachandar1@gmail.com>
2022-01-03 19:43:10 -0700

    adding recursion appraoch to 21

--- Python/21. Merge two sorted list.py
+++ Python/21. Merge two sorted list.py
@@ -33,3 +33,16 @@ class Solution(object):
             current = current.next
         current.next = l1 or l2
         return start.next
+
+        # recursion approach
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
+        
+        if list1.val <= list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
